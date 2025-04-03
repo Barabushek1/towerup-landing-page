b04085cd2cff0027799c4ea76924c58ddb861104
Barabushek1 <dasistreich@gmail.com>
2025-04-03 22:44:53 +0500

    Update ProjectsSection.tsx

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -230,7 +230,7 @@ const ProjectsSection: React.FC = () => {
             description: "Современный эко-комплекс из 5 домов с благоустроенной территорией, детскими площадками и парковой зоной.",
             location: "Ташкент",
             status: "Строится",
-            imageUrl: "https://images.unsplash.com/photo-1742855751015-5bda25456249?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            imageUrl: "/assets/Pushkin/18.jpg",
             slug: "pushkin"
         },
         {
